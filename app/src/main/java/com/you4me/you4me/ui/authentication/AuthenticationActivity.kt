8e80cceb4edcf85a6838e12cc7c97a7798dce19c
Bolaji Alabi <alabibolaji78@gmail.com>
2024-02-08 17:49:01 +0100

    login and registration ui

--- app/src/main/java/com/you4me/you4me/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/you4me/you4me/ui/authentication/AuthenticationActivity.kt
@@ -3,10 +3,14 @@ package com.you4me.you4me.ui.authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.you4me.you4me.R
+import com.you4me.you4me.databinding.ActivityAuthenticationBinding
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
