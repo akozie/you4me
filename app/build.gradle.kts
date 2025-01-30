beb9ee6c1995f0336e073c97d0600ad800ecb480
rhymezxcode <rhymezx.code@gmail.com>
2025-01-30 20:05:51 +0100

    release: pushed v. 1.31 ( 31 )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.you4me.you4me"
         minSdk = 24
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.30"
+        versionCode = 31
+        versionName = "1.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
