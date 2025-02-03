474ab6e29c4e6508e1f1b267776904aee4e1cadb
Akozi Emmanuel <akoziemmanuel@gmail.com>
2025-02-03 19:32:24 +0100

    mixpanel

--- app/src/main/jni/api-keys.c
+++ app/src/main/jni/api-keys.c
@@ -7,3 +7,8 @@ JNIEXPORT jstring JNICALL
 Java_com_you4me_you4me_utils_UtilityParam_getGooglePlacesSecretKey(JNIEnv *env, jobject thiz) {
     return (*env)->NewStringUTF(env, "AIzaSyAjzOVHaTnlRwL6ArgGsRXOAt3PCJ5FujI");
 }
+
+JNIEXPORT jstring JNICALL
+Java_com_you4me_you4me_utils_UtilityParam_getMixpanelSecretKey(JNIEnv *env, jobject thiz) {
+    return (*env)->NewStringUTF(env, "d1800cfe7956f69cf896a35dd8653f17");
+}
