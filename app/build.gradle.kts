b5df673cd568abc61986ae6dc677044a5409dc05
rhymezxcode <rhymezx.code@gmail.com>
2025-02-05 19:23:14 +0100

    feat: Update app version to 1.36
    - Increments `versionCode` from 35 to 36.
    - Updates `versionName` from "1.35" to "1.36".
    - Updates the app `build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.you4me.you4me"
         minSdk = 24
         targetSdk = 34
-        versionCode = 35
-        versionName = "1.35"
+        versionCode = 36
+        versionName = "1.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
